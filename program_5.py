363dcb17acf04a94ae2c1170f307c33debb4672a
Malo0423 <mlorenson@students.unwsp.edu>
2024-09-23 23:10:08 -0500

    Add files via upload
    Author: Maggie Lorenson
    Date: 9/23/2024
    Title: Week 3 Program 5

--- program_5.py
+++ program_5.py
@@ -1,6 +1,42 @@
-# There are two kinds of hot dogs sold:  
-# Hot Dog ($3.50), Chili Dog ($4.50).  
-# Additionally a person can order cheese ($0.50), peppers ($0.75), or grilled onions ($1.00).  
-# Also tax is 7%. 
-# Write a program the inputs the type of hot dog wanted and optional toppings.  
-# The program then displays the hot dog cost, tax and total cost. 
+hotdog = 3.50
+chilidog = 4.50
+cheese = 0.50
+peppers = 0.75
+grilled_onions = 1.00
+tax = 0.07
+
+
+
+
+hotdogOrder = (input('Do you want a hotdog or a chilidog? '))
+if "hotdog" in hotdogOrder:
+    hotdogOrder = 3.50
+else:
+    if "chilidog" in hotdogOrder:
+        hotdogOrder = 4.50
+cheeseOrder = (input('Do you want cheese? '))
+if "yes" in cheeseOrder:
+    cheeseOrder = 0.50
+else:
+    if "no" in cheeseOrder:
+        cheeseOrder = 0.00
+pepperOrder = (input('Do you want peppers? '))
+if "yes" in pepperOrder:
+    pepperOrder = 0.75
+else:
+    if "no" in pepperOrder:
+        pepperOrder = 0.00
+onionOrder = (input('Do you want grilled onions? '))
+if "yes" in onionOrder:
+    onionOrder = 1.00
+else:
+    if "no" in onionOrder:
+        onionOrder = 0.00
+
+
+subtotal = hotdogOrder + cheeseOrder + pepperOrder + onionOrder
+sales_tax = subtotal * tax
+total = subtotal + sales_tax
+print('Your subtotal is $', format(subtotal, ',.2f'),)
+print('Your tax is $', format(sales_tax, ',.2f'),)
+print('\n\nYour total is $', format(total, ',.2f'),)
